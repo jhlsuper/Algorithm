9f13a02a3d49c0b62b8f7e76b67a38a06cc95062
Jeff Lee <jhlsuper@gmail.com>
2021-02-18 02:14:33 +0900

    to 1045 done

---
+++ codeup/1to100/1036.py
@@ -0,0 +1,4 @@
+a = str(input())
+
+print(ord(a))
+# 아스키 코드를 바꾸는법 str을 ord로 한다.
---
+++ codeup/1to100/1045.py
@@ -0,0 +1,8 @@
+a, b = map(int, input().split())
+c = a/b
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a % b)
+print('%.02f' % c)
