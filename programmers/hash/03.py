153884ae8711c2ce996ee6e8dcd67b7ad496f23e
Jeff Lee <jhlsuper@gmail.com>
2021-04-14 21:34:35 +0900

    해쉬 3번문제 완료

--- programmers/hash/03.py
+++ programmers/hash/03.py
@@ -4,27 +4,18 @@ from typing import Dict
 def solution(clothes):
     dictionary = {}
     gop = 1
-    
+
     for i in clothes:
         if i[-1] in dictionary:
-            dictionary[i[-1]]+=1
+            dictionary[i[-1]] += 1
         else:
-            dictionary[i[-1]]=1
+            dictionary[i[-1]] = 1
     # print(dictionary)
     for val in dictionary.values():
-        gop=gop*val
-    
-    if len(dictionary)==1:
-        return(gop)
-    else:
-        return(len(clothes)+gop)
+        gop *= val+1
 
-    return gop
-             
-    
-    # answer = 0
-    # return answer
+    return gop-1  # 전부 안입는 경우를 뺀다.
 
 
-solution([["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], 
-["green_turban", "headgear"]])
+solution([["yellowhat", "headgear"], ["bluesunglasses", "eyewear"],
+          ["green_turban", "headgear"]])
