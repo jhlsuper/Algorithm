9f6721c07597884e3baf0dacbcdf17b8db9a4c17
Jeff Lee <jhlsuper@gmail.com>
2021-04-21 22:21:10 +0900

    해쉬 4번째 문제

--- programmers/hash/04.py
+++ programmers/hash/04.py
@@ -1,36 +1,51 @@
 import operator
+from collections import defaultdict
 
+
+# def solution(genres, plays):
+
+#     answer = []
+#     dict = {}
+#     value_list = []
+#     temp = []
+
+#     for i in range(len(genres)):
+#         # print(i) 0,1,2,3,4
+#         if genres[i] not in dict:
+#             dict[genres[i]] = [plays[i]]
+
+#         elif len(dict[genres[i]]) == 1:
+#             dict[genres[i]].append(plays[i])
+#         elif len(dict[genres[i]]) == 2:
+#             if min(dict[genres[i]]) >= plays[i]:
+#                 pass
+#             else:
+#                 dict[genres[i]].append(plays[i])
+#                 dict[genres[i]].remove(min(dict[genres[i]]))
+
+#     for j in dict.values():
+#         value_list.append(sorted(j, reverse=True))
+
+#     c = sorted(value_list, key=lambda x: -max(x[0], x[1]))
+#     # print(c)
+#     for i in (c):
+#         for j in range(len(i)):
+#             temp.append(i[j])
+#     # print(temp)
+#     for i in temp:
+#         answer.append(plays.index(i))
+#     # print(answer)
+#     return answer
 def solution(genres, plays):
-    
     answer = []
-    dict = {}
-
-    for i in range(len(genres)):
-        #print(i) 0,1,2,3,4
-        if genres[i] not in dict:
-            dict[genres[i]]= [plays[i]]
-            
-        elif len(dict[genres[i]])==1:
-            dict[genres[i]].append(plays[i])
-        elif len(dict[genres[i]])==2:
-            if min(dict[genres[i]]) >=plays[i]:
-                pass
-            else:
-                dict[genres[i]].append(plays[i])
-                dict[genres[i]].remove(min(dict[genres[i]]))
-    print(dict)
-    # for i in dict:
-        # print(max(dict[i]))
-    # sorted_dict = sorted(dict.items(),key = operator.itemgetter(1))
-
-    print(dict)
+    d = {e:[] for e in set(genres)}
+    for e in zip(genres, plays, range(len(plays))):
+        d[e[0]].append([e[1] , e[2]])
+    genreSort =sorted(list(d.keys()), key= lambda x: sum(map(lambda y: y[0], d[x])), reverse = True)
+    for g in genreSort:
+        temp = [e[1] for e in sorted(d[g],key= lambda x: (x[0], -x[1]), reverse = True)]
+        answer += temp[:min(len(temp),2)]
     return answer
 
-##딕셔너리에 2차원배열을 넣어서 2개 까지 넣는다.
-##장르별 베스트 앨범 출력 
-##속한 노래가 많이 재생된 장르 
-##장르내에서 가장많이 
-## 고유번호 낮은게 우선
-##2개의 출력값 
-
-solution(["classic", "pop", "classic", "classic", "pop"],[500, 600, 150, 800, 2500])
+solution(["classic", "pop", "classic", "classic", "pop"],
+         [500, 600, 150, 800, 2500])
