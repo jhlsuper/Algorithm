0c50b45d5e9bf81e97a5d7c378a914a8d04684a8
Jeff Lee <jhlsuper@gmail.com>
2021-04-25 00:10:19 +0900

    스택 1번째 문제

---
+++ programmers/stackorqueue/1.py
@@ -0,0 +1,24 @@
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+
+    list = [0]*bridge_length
+
+    while list:
+        answer += 1
+        list.pop(0)
+        print(list)
+        if truck_weights:
+            if sum(list) + truck_weights[0] <= weight:
+                list.append(truck_weights.pop(0))
+                # print(list)
+
+            else:
+                list.append(0)
+                # print(list)
+
+    # print(answer)
+
+    return answer
+
+
+solution(2, 10, [7, 4, 5, 6])
