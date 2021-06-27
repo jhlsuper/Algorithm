f77fdd6f44ff1fc37e714b51adb4f38acf3332f6
Jeff Lee <jhlsuper@gmail.com>
2021-06-28 00:46:59 +0900

    카카오 2018 비밀지도 문제 done

--- programmers/kakao/2018_1차_비밀지도.py
+++ programmers/kakao/2018_1차_비밀지도.py
@@ -1,11 +1,12 @@
 def solution(n, arr1, arr2):
     answer = []
 
-    map_array = [[0]*n for i in range(n)]
-    print(map_array)
-
-
+    for i, j in zip(arr1, arr2):
+        binary = str(bin(i | j)[2:])
+        binary = binary.rjust(n, '0')
+        binary = binary.replace("1", '#')
+        binary = binary.replace('0', ' ')
+        answer.append(binary)
 
     return answer
-
 solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28])
