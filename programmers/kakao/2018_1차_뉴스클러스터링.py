b4961752c33935e3e03118e2ff4a649162a7219b
Jeff Lee <jhlsuper@gmail.com>
2021-06-28 20:55:58 +0900

    뉴스 문제 완료/ 버스 문제풀어야됨

--- programmers/kakao/2018_1차_뉴스클러스터링.py
+++ programmers/kakao/2018_1차_뉴스클러스터링.py
@@ -4,8 +4,6 @@ def solution(str1, str2):
     str1_list = makelist(str1)
     str2_list = makelist(str2)
 
-    # print(str1_list)
-    # print(str2_list)
     answer = cal(str1_list, str2_list)
     print(answer)
     return answer
@@ -14,7 +12,7 @@ def solution(str1, str2):
 def makelist(str):
     str_list = []
     for i in range(len(str)):
-        # print(str1[i:i + 2])
+
         if str[i:i + 2].isalpha() and len(str[i:i + 2]) == 2:
             str_list.append(str[i:i + 2])
     return str_list
@@ -23,16 +21,14 @@ def makelist(str):
 def cal(list1, list2):
     jaccard = 0
 
-    inter_list, union_list = inter_union(list1,list2)
-    # print(len(union_list), union_list)
-    # print(len(inter_list), inter_list)
-    if len(inter_list) == 0:
-        jaccard = 1
-        return jaccard*65536
-    # if union_list == intersection_list:
-    #     jaccard = max(len(list1), len(list2)) / (len(list1) + len(list2))
-    #     print(jaccard)
-    #     return int(jaccard * 65536)
+    inter_list, union_list = inter_union(list1, list2)
+
+    if len(inter_list) and len(union_list) == 0:
+        jaccard = 1 * 65536
+        return jaccard
+    if len(union_list) == 0:
+        jaccard = 1 * 65536
+        return jaccard
     else:
         jaccard = len(inter_list) / len(union_list)
         return int(jaccard * 65536)
@@ -51,13 +47,13 @@ def union(list1, list2):
 
 
 def inter_union(list1, list2):
-    if len(list1)>len(list2):
+    if len(list1) > len(list2):
         intersect_list = [list1.remove(i) for i in list2 if i in list1]
     else:
-        intersect_list=[list2.remove(i) for i in list1 if i in list2]
+        intersect_list = [list2.remove(i) for i in list1 if i in list2]
 
-    union_list = list1+list2
-    return intersect_list,union_list
+    union_list = list1 + list2
+    return intersect_list, union_list
 
 
 solution("aa1+aa2", "AAAA12")
---
+++ programmers/kakao/2018_1차_셔틀버스.py
@@ -0,0 +1,7 @@
+def solution(n, t, m, timetable):
+    answer = ''
+
+    return answer
+
+
+solution(1, 1, 5, ["08:00", "08:01", "08:02", "08:03"])
