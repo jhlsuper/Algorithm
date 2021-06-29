d25173846fdf08963a532d26f96bdfc774b940e1
Jeff Lee <jhlsuper@gmail.com>
2021-06-30 00:40:18 +0900

    프렌즈 4블록 5,6,10,11 케이스 실패

--- programmers/kakao/2018_1차_프렌즈4블록.py
+++ programmers/kakao/2018_1차_프렌즈4블록.py
@@ -7,32 +7,46 @@ def solution(m, n, board):
         for j in range(n):
             board_list[i][j] = board[i][j]
 
-    print(board_list)
+    board_list, answer = del_block(m, n, board_list, answer)
 
-    del_block(m, n, board_list)
-
+    board_list = move_down(m, n, board_list)
+    board_list, answer = del_block(m, n, board_list, answer)
+    board_list = move_down(m, n, board_list)
+    board_list, answer = del_block(m, n, board_list, answer)
+    board_list = move_down(m, n, board_list)
+    board_list, answer = del_block(m, n, board_list, answer)
+    board_list = move_down(m, n, board_list)
+    # print(answer)
     return answer
 
 
-def del_block(m, n, board):
+def del_block(m, n, board, answer):
     temp_list = []
     for i in range(m - 1):
         for j in range(n - 1):
-            if board[i][j] == board[i][j + 1]:
+            if board[i][j] == board[i][j + 1] and board[i][j] != -1:
                 # print(board[i][j:j + 2])
                 if board[i][j:j + 2] == board[i + 1][j:j + 2]:
-                    print(board[i][j:j + 2], board[i + 1][j:j + 2])
+
                     for x in range(i, i + 2):
                         for y in range(j, j + 2):
                             if [x, y] not in temp_list:
                                 temp_list.append([x, y])
+                                answer += 1
 
-                    print(temp_list)
     for i in temp_list:
-        # print(i[0], i[1])
-        board[i[0]][i[1]] = '0'
+        board[i[0]][i[1]] = -1
+    return board, answer
 
-    print(board)
+
+def move_down(m, n, board):
+    for i in range(m-1, 0,-1):
+        for j in range(n):
+            # print(i, j)
+            if board[i][j] == -1:
+                board[i][j] = board[i-1][j]
+                board[i-1][j] = -1
+    return board
 
 
 solution(4, 5, ["CCBDE", "AAADE", "AAABF", "CCBBF"])
