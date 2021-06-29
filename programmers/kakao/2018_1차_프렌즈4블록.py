a2c5c06b2534e7a774301ffea5601ec07e910743
Jeff Lee <jhlsuper@gmail.com>
2021-06-30 00:53:20 +0900

    프렌즈4블록 5,10test fail

--- programmers/kakao/2018_1차_프렌즈4블록.py
+++ programmers/kakao/2018_1차_프렌즈4블록.py
@@ -2,20 +2,37 @@ def solution(m, n, board):
     answer = 0
 
     board_list = [[0] * n for row in range(m)]
-
+    a = 0
     for i in range(m):
         for j in range(n):
             board_list[i][j] = board[i][j]
+    while a < m:
+        temp_list = []
+        for i in range(m - 1):
+            for j in range(n - 1):
+                if board_list[i][j] == board_list[i][j + 1] and board_list[i][j] != -1:
+                    # print(board[i][j:j + 2])
+                    if board_list[i][j:j + 2] == board_list[i + 1][j:j + 2]:
 
-    board_list, answer = del_block(m, n, board_list, answer)
+                        for x in range(i, i + 2):
+                            for y in range(j, j + 2):
+                                if [x, y] not in temp_list:
+                                    temp_list.append([x, y])
+                                    answer += 1
 
-    board_list = move_down(m, n, board_list)
-    board_list, answer = del_block(m, n, board_list, answer)
-    board_list = move_down(m, n, board_list)
-    board_list, answer = del_block(m, n, board_list, answer)
-    board_list = move_down(m, n, board_list)
-    board_list, answer = del_block(m, n, board_list, answer)
-    board_list = move_down(m, n, board_list)
+        for i in temp_list:
+            board_list[i[0]][i[1]] = -1
+        move_down(m, n, board_list)
+        a += 1
+    # board_list, answer = del_block(m, n, board_list, answer)
+    #
+    # board_list = move_down(m, n, board_list)
+    # board_list, answer = del_block(m, n, board_list, answer)
+    # board_list = move_down(m, n, board_list)
+    # board_list, answer = del_block(m, n, board_list, answer)
+    # board_list = move_down(m, n, board_list)
+    # board_list, answer = del_block(m, n, board_list, answer)
+    # board_list = move_down(m, n, board_list)
     # print(answer)
     return answer
 
@@ -40,12 +57,12 @@ def del_block(m, n, board, answer):
 
 
 def move_down(m, n, board):
-    for i in range(m-1, 0,-1):
+    for i in range(m - 1, 0, -1):
         for j in range(n):
             # print(i, j)
             if board[i][j] == -1:
-                board[i][j] = board[i-1][j]
-                board[i-1][j] = -1
+                board[i][j] = board[i - 1][j]
+                board[i - 1][j] = -1
     return board
 
 
