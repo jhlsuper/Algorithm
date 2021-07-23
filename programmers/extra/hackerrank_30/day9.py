9af42653e68d8cee4dd4c9a4013c820e304a9c5e
Jeff Lee <jhlsuper@gmail.com>
2021-07-23 23:57:12 +0900

    hanker rank day 9,10 done

--- programmers/extra/hackerrank_30/day9.py
+++ programmers/extra/hackerrank_30/day9.py
@@ -1,9 +1,10 @@
 def factorial(n):
     if n == 1:
         return 1
-    elif n ==0:
+    elif n == 0:
         return 0
     else:
-        return n*factorial(n-1)
+        return n * factorial(n - 1)
+
 
 factorial(3)
