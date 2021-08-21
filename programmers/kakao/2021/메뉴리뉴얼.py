41b05ea51c437ad838d03e9b95a13c168e430441
jhlsuper <jhlsuper@gmail.com>
2021-08-21 23:40:50 +0900

    프로그래머스 메뉴리뉴얼 문제 not done

--- programmers/kakao/2021/메뉴리뉴얼.py
+++ programmers/kakao/2021/메뉴리뉴얼.py
@@ -1,3 +1,17 @@
+from string import ascii_uppercase
+
+
 def solution(orders, course):
+    dict ={}
+
+    a = list(ascii_uppercase)
+    for i in orders:
+        for index in range(2,len(i)):
+            print(i[])
+
+
     answer = []
     return answer
+
+
+solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2, 3, 4])
