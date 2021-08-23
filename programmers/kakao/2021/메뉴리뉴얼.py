e1fbc1ed5fa501dbb4b0663090ee892749c8894f
Jeff Lee <jhlsuper@gmail.com>
2021-08-23 21:42:12 +0900

    카카오 메뉴리뉴얼

--- programmers/kakao/2021/메뉴리뉴얼.py
+++ programmers/kakao/2021/메뉴리뉴얼.py
@@ -1,17 +1,27 @@
-from string import ascii_uppercase
+from itertools import combinations
+from collections import Counter
 
 
 def solution(orders, course):
-    dict ={}
-
-    a = list(ascii_uppercase)
-    for i in orders:
-        for index in range(2,len(i)):
-            print(i[])
-
-
     answer = []
-    return answer
+
+    for n in course:
+        array = []
+        for o in orders:
+            order = sorted(o)
+            # print(list(combinations(order, n)))
+            array.extend(list(combinations(order, n)))
+
+        count = Counter(array).most_common()
+
+        for index in range(len(count)):
+            temp_max = count[0][1]
+            if count[index][1] >= 2 and count[index][1] == temp_max:
+                answer.append("".join(count[index][0]))
+            else:
+                break
+
+    return sorted(answer)
 
 
 solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2, 3, 4])
