1afabef87c83e689fcfb0b48d081415a923cf89b
jhlsuper <jhlsuper@gmail.com>
2022-08-08 00:51:40 +0900

    boj 9012 solved

---
+++ BJ/Stack/9012.py
@@ -0,0 +1,28 @@
+import sys
+def solove(input):
+    stack =[]
+    for i in input :
+        if(i=='('):
+            stack.append('(')
+        else:
+            if(len(stack)>0):
+                
+                if("(" in stack):
+                    stack.pop()
+                            
+            else:
+                stack.append(')')
+    # print(stack)
+    if(len(stack)==0):
+        print("YES")
+    else:
+        print("NO")
+            
+
+a = int(sys.stdin.readline())
+data = [sys.stdin.readline().strip() for i in range(a)]
+for i in data:
+    solove(i)
+
+
+        
