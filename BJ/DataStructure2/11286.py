ff2b4d1fb17cf9eafb2d0c5bf2f7c4b83810e5d9
jhlsuper <jhlsuper@gmail.com>
2022-09-01 23:45:07 +0900

    BOJ 11286 solved, heapq

---
+++ BJ/DataStructure2/11286.py
@@ -0,0 +1,15 @@
+import sys 
+import heapq
+N = int(sys.stdin.readline())
+
+heap =[]
+
+for i in range(N):
+    num = int(sys.stdin.readeline())
+    if(num!= 0):
+        heapq.heappush(heap,(abs(num),num))
+    else:
+        try:
+            print(heapq.heappop(heap)[1])
+        except:
+            print(0)
