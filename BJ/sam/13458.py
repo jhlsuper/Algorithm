45a0f609b133b07514cf826b5c6e593d7bafa68d
jhlsuper <jhlsuper@gmail.com>
2022-10-11 16:18:42 +0900

    BOJ 13458 done

--- BJ/sam/13458.py
+++ BJ/sam/13458.py
@@ -3,6 +3,24 @@ import sys
 N = int(sys.stdin.readline())
 A = list(map(int, sys.stdin.readline().split(' '))) 
 B,C = map(int, sys.stdin.readline().split(' '))
+answer =[]
+# print(N,A,B,C)
 
-print(N,A,B,C)
+for i in A:
+    count =0
+    
+    
+    i=i-B;
+    count+=1
+    if(i>0):
+        count+= i//C
+        if(i%C)!=0:
+            count +=1
+    
+    answer.append(count)
 
+print(sum(answer))
+
+        
+        
+    
