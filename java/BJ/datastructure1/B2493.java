d3e7b216acffe9597882d97d583c4639fc64426a
jhlsuper <jhlsuper@gmail.com>
2023-01-14 16:44:51 +0900

    java input omg

---
+++ java/BJ/datastructure1/B2493.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.io.IOException;
+
+
+public class B2493 {
+    public static void main(String[] args) {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = br.readLine();
+        
+        
+    }
+}
---
+++ java/BJ/datastructure1/B2800.java
@@ -0,0 +1,24 @@
+//스도쿠
+
+import java.util.*;
+
+public class B2800 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String a = sc.nextLine();
+        String[] arr = a.split("");
+        Stack<String> stack = new Stack<>();
+
+        // for (String i:arr){
+        //     // System.out.println(i);
+        //     if(i.equals("(") || i.equals(")")){
+        
+        //         stack.push(i);
+        //     } 
+        // }
+        
+        // System.out.printf("%d", arr.length);
+        
+    }
+}
