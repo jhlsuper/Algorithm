3a7e268697435628d4328810bc338118b31ecdfe
jhlsuper <jhlsuper@gmail.com>
2023-01-14 22:21:21 +0900

    b14891 redo

---
+++ java/BJ/SWA/B14891.java
@@ -0,0 +1,15 @@
+package SWA;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class B14891 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str; 
+        
+        str = br.readLine();
+        System.out.println(str);
+
+    }
+}
