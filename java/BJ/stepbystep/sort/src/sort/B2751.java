3f3c7b88655c0bae018e290d9f764fbd12665273
jhlsuper <jhlsuper@gmail.com>
2023-01-29 20:19:45 +0900

    2751 solved

--- java/BJ/stepbystep/sort/src/sort/B2751.java
+++ java/BJ/stepbystep/sort/src/sort/B2751.java
@@ -1,9 +1,30 @@
 package sort;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class B2751  {
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	int n = Integer.parseInt(br.readLine());
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		ArrayList<Integer> arr = new ArrayList<>();
+		StringBuilder sb = new StringBuilder();
+		for (int i=0;i<n;i++){
+			int temp = Integer.parseInt(br.readLine());
+//			System.out.println(temp);
+			arr.add(temp);
+		}
+//		Arrays.sort(arr);
+		Collections.sort(arr);
+		for(int e : arr){
+
+			sb.append(e).append('\n');
+		}
+		System.out.println(sb);
+	}
+
 }
