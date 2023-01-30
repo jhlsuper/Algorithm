a275d9f170e2418836913538e2bc6bdad97a8442
jhlsuper <jhlsuper@gmail.com>
2023-01-30 23:36:25 +0900

    sort little solved

--- java/BJ/stepbystep/sort/src/sort/B11650.java
+++ java/BJ/stepbystep/sort/src/sort/B11650.java
@@ -4,16 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class B11650  {
+public class B11650 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		for(int i = 0;i<N;i++){
+		for (int i = 0; i < N; i++) {
 			String temp = br.readLine();
 
 			int a = Integer.parseInt(temp.split(" ")[0]);
 			int b = Integer.parseInt(temp.split(" ")[1]);
+			System.out.println(a, b);
 		}
 	}
-	
+
 }
--- java/BJ/stepbystep/sort/src/sort/B2108.java
+++ java/BJ/stepbystep/sort/src/sort/B2108.java
@@ -7,53 +7,59 @@ import java.util.Arrays;
 
 public class B2108 {
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-		int[] count = new int[8002];
-		int sum = 0;
-		int max = Integer.MAX_VALUE;
-		int min = Integer.MIN_VALUE;
-		int mid = 10000;
-		int mode =10000;
-		for (int i = 0; i < N; i++) {
-			int temp = Integer.parseInt(br.readLine());
-			sum+= temp;
-			arr[temp+4000] ++;
-			if(max<temp) {
-				max = temp;
-			}
-			if(min>temp) {
-				min = temp;
-			}
-			
-		}
-		for (int i = 0; i < 8002; i++) {
-			if (count[i] > max) {
-				max = count[i];
-				max_count = i;
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] count = new int[8002];
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int mid = 10000;
+        int mode = 10000;
 
-			}
-		}
-		for(int i=0; i<8002; i++) {
-			if(count[i]==max) {
-				
-				counter+=1;
-				answer =i;
-			
-			}
-			if(counter ==2) {
-				break;
-			}
-		}
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            int temp = Integer.parseInt(br.readLine());
+            sum += temp;
+//			arr[i] =temp;
+            if (max < temp) {
+                max = temp;
+            }
+            if (min > temp) {
+                min = temp;
+            }
+            count[4000 + temp]++;
 
-		Arrays.sort(arr);
-		System.out.println(sum / N);
-		System.out.println(arr[(N + 1) / 2 - 1]);
-		System.out.println(answer-4001);
-		System.out.println(arr[arr.length - 1] - arr[0]);
-	}
+        }
+        int cnt = 0;
+        int max_count = 0;
+        boolean flag = false;
+        for (int i = min + 4000; i <= max + 4000; i++) {
+            if (count[i] > 0) {
+
+                if (cnt < (N + 1) / 2) {
+                    cnt += count[i];
+                    mid = i - 4000;
+                }
+                if (max_count < count[i]) {
+                    max_count = count[i];
+                    mode = i - 4000;
+                    flag = true;
+                } else if (max_count == count[i] && flag) {
+                    mode = i - 4000;
+                    flag = false;
+                }
+
+            }
+        }
+
+        Arrays.sort(arr);
+        System.out.println((int) Math.round((double) sum / N));
+        System.out.println(mid);
+        System.out.println(mode);
+        System.out.println(max-min);
+    }
 
 }
