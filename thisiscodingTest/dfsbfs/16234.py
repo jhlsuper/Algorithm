a19fe9b2747e84460811d0850538a0b4fc585b86
이재현 <jhlsuper@gmail.com>
2023-02-22 08:56:45 +0900

    16234

--- thisiscodingTest/dfsbfs/16234.py
+++ thisiscodingTest/dfsbfs/16234.py
@@ -5,61 +5,61 @@
 
 ## 두칸의 차이가 사이인거 전부 마킹 값 배열에 넣기 모두 더해서 나눠주기
 ## 나누어져 있으면 배열값에 배열로 추가
+import math
+from collections import deque
+
 global visited
 global answer
-global visited
+# global visited
 global opened
 N, L, R = map(int, input().split())
 
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
-visited = []
 opened = []
 x, y = 0, 0
 answer = 0
-flag = True
 
 
-def dfs(x, y):
-    print(opened)
-    for i in range(4):
-        newX = x + dx[i]
-        newY = y + dy[i]
+def bfs(a, b):
+    q = deque()
+    temp = []
+    q.append((a, b))
+    temp.append((a, b))
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            newX = x + dx[i]
+            newY = y + dy[i]
 
-        if 0 <= newX < N and 0 <= newY < N:
+            if 0 <= newX < N and 0 <= newY < N and visited[newX][newY] == 0:
 
-            if L <= abs(arr[x][y] - arr[newX][newY]) <= R:
-                opened.append((newX, newY))
-            if (newX, newY) not in visited:
-                visited.append((newX, newY))
-                dfs(newX, newY)
+                if L <= abs(arr[x][y] - arr[newX][newY]) <= R:
+                    visited[newX][newY] = 1
+                    q.append((newX, newY))
+                    temp.append((newX, newY))
 
-
-def move():
-    sum = 0
-    global answer
-    if len(opened) > 0:
-        answer += 1
-        for a, b in opened:
-            sum += arr[a][b]
-
-        each = int(sum / len(visited))
-        for a, b in opened:
-            arr[a][b] = each
-        opened.clear()
-        visited.clear()
-        return True
-    else:
-        return False
+    return temp
 
 
-# while flag:
-dfs(0, 0)
-print(arr)
-move()
-print(arr)
+while 1:
+    visited = [[0] * (N + 1) for _ in range(N + 1)]
+    flag = 0
+    for i in range(N):
+        for j in range(N):
+            if visited[i][j] == 0:
+                visited[i][j] = 1
+                country = bfs(i, j)
+                if len(country) > 1:
+                    flag = 1
+                    number = sum([arr[x][y] for x, y in country]) // len(country)
+                    for x, y in country:
+                        arr[x][y] = number
+    if flag == 0:
+        break
+    answer += 1
 
 print(answer)
