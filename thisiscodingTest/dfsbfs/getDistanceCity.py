82e4f84494f0b0f362556d5e942c2f73b270b04a
이재현 <jhlsuper@gmail.com>
2023-02-23 10:55:20 +0900

    getDistance

---
+++ thisiscodingTest/dfsbfs/getDistanceCity.py
@@ -0,0 +1,40 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+N, M, K, X = map(int, input().split())
+
+graph = [[] for _ in range(N + 1)]
+visited = [False] * (N + 1)
+distance = [0] * (N + 1)
+# print(graph)
+
+for i in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+
+def bfs(start):
+    answer = []
+    q = deque([start])
+    visited[start] = True
+    distance[start] = 0
+    while q:
+        now = q.popleft()
+        for i in graph[now]:
+            if not visited[i]:
+                visited[i] = True
+                q.append(i)
+                distance[i] = distance[now] + 1
+                if distance[i] == K:
+                    answer.append(i)
+                    # print(answer)
+    if (len(answer)) == 0:
+        print(-1)
+        return
+    else:
+        answer.sort()
+        for i in answer:
+            print(i)
+
+
+bfs(X)
