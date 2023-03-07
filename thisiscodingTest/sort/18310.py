3cc5bae259bc61232452ca88d91d50124dc757ab
Jeff LEe <jhlsuper@gmail.com>
2023-03-07 09:28:49 +0900

    bj 18310 how to solve?

---
+++ thisiscodingTest/sort/18310.py
@@ -0,0 +1,29 @@
+N = int(input())
+
+answer = 1e9
+li = list(map(int, input().split()))
+# print(li)
+# print(sorted(li))
+li = sorted(li)
+if len(li) != 1:
+    length = li[-1] - li[0]
+
+# print(int(length / 2))
+mid = int(length / 2) + li[0]
+if mid in li:
+    answer = mid
+    print(answer)
+else:
+
+    while True:
+        mid_up = mid + 1
+        mid_down = mid - 1
+        print(mid_down, mid_up)
+        if mid_down in li:
+            answer = mid_down
+            break
+        if mid_up in li:
+            # print(mid_up, mid_down)
+            answer = mid_up
+            break
+    print(answer)
