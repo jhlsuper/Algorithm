4f5c424a5aaae620e7e79b6054f4872996915d7a
Jeff LEe <jhlsuper@gmail.com>
2023-03-09 17:50:04 +0900

    1715 cant solve

--- thisiscodingTest/sort/1715.py
+++ thisiscodingTest/sort/1715.py
@@ -6,13 +6,18 @@ for i in range(N):
 answer = 0
 li = sorted(li)
 
-for i in range(len(li) - 1):
-    temp = 0
-    # print("ii", i)
-    # print(li)
-    temp = li[i] + li[i + 1]
-    li[i + 1] = temp
-    # print(temp)
-    answer += temp
+if len(li) == 1:
+    print(0)
+    exit(0)
+else:
+
+    for i in range(len(li) - 1):
+        temp = 0
+        print("ii", i)
+        print(li)
+        temp = li[i] + li[i + 1]
+        li[i + 1] = temp
+        # print(temp)
+        answer += temp
 
 print(answer)
