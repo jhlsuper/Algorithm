12fb15672c1e96b9920feb231675d820928c88ba
Jeff LEe <jhlsuper@gmail.com>
2023-04-05 10:21:52 +0900

    sw2112

--- thisiscodingTest/implement/sw2112.py
+++ thisiscodingTest/implement/sw2112.py
@@ -1,45 +1,76 @@
 import sys
 from itertools import combinations
 
-def checkD(maps, k):
-    totalCnt = 0
-    for i in range(w):
-
-        ##0이면 A 1이면 B
-        flag = maps[0][i]
-        count = 1
-        for j in range(1, d):
-            index = maps[j][i]
-            if index == flag:
-                count += 1
-            else:
-                flag = index
-                count = 1
-            if count >= k:
-                totalCnt += 1
-                break
-    if totalCnt == w:
-        return True
-    else:
-        return False
-
-
-T = int(input())
 global d
 global w
 global k
-for tc in range(T):
+global max
+global maps
+global countli
+
+
+def checkD():
+    # print
+    global maps
+    global countli
+    # print(countli)
+    totalCnt = 0
+
+    for i in range(w):
+
+        ##0이면 A 1이면 B
+
+        count = 1
+        for j in range(d-1):
+            a = maps[j][i]
+            b = maps[j + 1][i]
+            if countli[j] != -1:
+                a = countli[j]
+            if countli[j + 1] != -1:
+                b = countli[j + 1]
+            if a == b:
+                count += 1
+                if count >= k:
+                    break
+
+            else:
+                count = 1
+        if count < k:
+            return False
+    return True
+
+
+def dfs(idx, cnt):
+    global max
+    if cnt > max:
+        return
+    if idx == d:
+        if checkD():
+            if cnt < max:
+                max = cnt
+        return
+
+    # temp = countli[idx]
+    countli[idx] = -1
+    dfs(idx + 1, cnt)
+    # countli[idx] = temp
+    countli[idx] = 0
+    dfs(idx + 1, cnt + 1)
+    # countli[idx] = temp
+    countli[idx] = 1
+    dfs(idx + 1, cnt + 1)
+    # countli[idx] = temp
+
+
+T = int(input())
+
+for tc in range(1, T + 1):
     d, w, k = map(int, input().split())
     ## 두께 w 가로크기
     maps = [list(map(int, input().split())) for _ in range(d)]
-    print( checkD(maps, k))
-    # print(maps)
-    if checkD(maps,k):
-        print(f"#{tc+1} 0")
-    else:
-        indexli =[]
-        for i in range(d):
-            indexli.append(i)
-        for i in range(1,d):
-            for c in combinations(indexli, i):
-                for a in c:
+
+    max = 1e9
+    countli = [-1] * d
+    # print(max)
+    dfs(0, 0)
+    print(f"#{tc} {max}")
